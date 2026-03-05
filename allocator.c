8c84a30361dfbee916d08df68a56b8da91126cd5
Aditya <adityadasch@gmail.com>
2026-03-05 12:25:42 +0530

    Fixed syntax errors

--- allocator.c
+++ allocator.c
@@ -1,6 +1,6 @@
-#include allocator.h
+#include "allocator.h"
 
-block_size = 512;
+extern block_size = 512;
 totalBlocks = 1024;
 reservedBlocks = 12;
 usableBlocks = totalBlocks - reservedBlocks;
@@ -21,20 +21,20 @@ void findFirstFreeIndex(int blockId,int payLoad, int *memoryIndex){
 	// Takes a blockId and finds the position in the block with payLoad number of free spaces
 	int blockIndex = blockId * block_size;
 	int flag = 0;
-	for (index = blockIndex; index<blockIndex+block_size, index++){
+	for (index = blockIndex; index<blockIndex+block_size; index++){
 		if(memory[index] == 0){
 			// Check if the byte at index is free
 			flag = 1;
-			for(int i = 1; i<payload; i++){
+			for(int i = 1; i<payLoad; i++){
 				// Iterate through the memory for free space
 				if(memory[index+i]!=0){
 					// Break out of the loop and set flag to 0 if non-continuous
 					flag = 0;
-					break
+					break;
 				}
 			}
 			if(flag == 1){
-				memoryIndex = index;
+				*memoryIndex = index;
 				return;
 			}
 		}		
@@ -42,14 +42,17 @@ void findFirstFreeIndex(int blockId,int payLoad, int *memoryIndex){
 	
 }
 
-int AllocateMemory(int blockId, byte value){
+int AllocateMemory(int blockId, byte[] value){
 	// Sets the first postion with payLoad(number of bytes) amount of free space with value
 	if (blockId > usableBlocks){
 		return 0;
 	}
 	int memoryIndex = 0;
-	findFirstFreeIndex(blockId,sizeof(value)/sizeof(value[0]), &memoryIndex);
-	memory[memoryIndex] = value;
+	int payLoad = sizeof(value)/sizeof(value[0]);
+	findFirstFreeIndex(blockId,payLoad , &memoryIndex);
+	for (int i = 0; i < payLoad; i++){
+		memory[memoryIndex+i] = value[i];
+	}
 	return 1;
 }
 
@@ -57,4 +60,5 @@ int FreeMemory(int byteAddr[2], int byteCount){
 	// [BlockId, ByteId], Number of bytes from that position to be removed
 	void *start = memory+(byteAddr[0]*block_size)+(byteAddr[1]);
 	memset(start, 0, byteCount);
+	return 1;
 }
--- allocator.h
+++ allocator.h
@@ -2,7 +2,7 @@
 #define HEADER
 
 #include <stdint.h>
-#include <stbool.h> 
+#include <stdbool.h> 
 #include <stdio.h>
 #include <string.h>
 
@@ -11,11 +11,8 @@ typedef uint8_t byte;
 // Global variables
 extern int block_size;
 extern byte *memory;
-
-#ifdef _WIN32 
+:
 #define DLL_EXPORT __declspec(dllexport) 
-#else 
-#define DLL_EXPORT #endif
 
 
 // Function prototypes
